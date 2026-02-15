92e8207156f40e9acd160c8d47a3fa33bfb15b77
GameMaster <kaka@gmail.com>
2026-02-15 21:39:59 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("CI/CD Kubernetes Demo v2");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
